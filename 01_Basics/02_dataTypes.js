ae2c2be50c4e716cc73b98154218fcbea7057fa8
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-11-24 16:22:56 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //Treat All JS code as newer version
+
+// alert(3 + 3) //We are using NodeJS not browser
+
+console.log(
+    3 + 3) //Code readability should be high
+
+console.log("Taher")
+
+let name = "Taher"
+let age = 21
+let isLoggedIn = false
+
+//Number => 2 to the power 53
+//bigint
+//string = ""
+//boolean = true/false
+//null = standalone value
+//undefined
+//symbol = unique
+
+
+//object
+
+console.log(typeof undefined) //(Output: undefined)
+console.log(typeof null) //(Output: object)
