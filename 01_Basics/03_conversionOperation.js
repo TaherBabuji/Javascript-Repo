28b6d48be14a4bad1b4770219ccd6c4842f5e97a
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-11-25 07:02:13 +0000

    Conversions and operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -1,12 +1,12 @@
 let score = "33"
 
-console.log(typeof score)
-console.log(typeof(score))
+// console.log(typeof score)
+// console.log(typeof(score))
 
 let valueInNumber = Number(score)
 
-console.log(typeof(valueInNumber))
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,7 +17,7 @@ console.log(valueInNumber)
 let isLoggedIn = "Taher"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1 => true
 // 0 => false
@@ -27,5 +27,38 @@ console.log(booleanIsLoggedIn)
 let someNumber = 32
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof(stringNumber))
+// console.log(stringNumber)
+// console.log(typeof(stringNumber))
+
+
+//%%%%%%%%%%%%%%%%%%%%%%%%%%    OPERATIONS   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+// console.log(2 + 2)
+// console.log(2 - 2)
+// console.log(2 * 2)
+// console.log(2 ** 2)
+// console.log(2 / 2)
+// console.log(2 % 2)
+
+let str1 = "Hello"
+let str2 = " Taher"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "3")
+// console.log("1" + 2 + 3)
+// console.log( 3 + 4 * 5 % 3)
+
+let num1,num2,num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100
+++gameCounter
+console.log(gameCounter)
