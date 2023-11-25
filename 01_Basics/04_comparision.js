df7d5fcd7d650be4219cb7c710768c2d34c36707
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-11-25 09:18:21 +0000

    Comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,21 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+
+
+console.log("2" === 2) //This is strict chech where datatype of values will also be checked
+console.log("2" == 2) //This is normal chech where datatype of values will not be checked
