eddb824e2baeff53e933ed4ebb924d73c2b3cbe0
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-11-27 10:47:15 +0000

    strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,34 @@
+const name = "Taher"
+const repoCount = 4
+
+// console.log(name + repoCount + " Value")
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`) //This method is known as string interpulation
+const gameName = new String("Taherbabuji")
+
+console.log(gameName[5])
+console.log(gameName.__proto__)
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(0))
+console.log(gameName.indexOf("e"))
+
+const newString = gameName.substring(0,5)
+console.log(newString)
+
+const newString2 = gameName.slice(-10,12)
+console.log(newString2)
+
+const newString3 = "      Taher        "
+console.log(newString3)
+console.log(newString3.trim())
+
+const url = "https://taher.com/taher%20babuji"
+
+console.log(url.replace("%20", "-"))
+
+console.log(url.includes("taherr"))
+
+const newString4 = "Taher-Aliasger-Babuji"
+
+console.log(newString4.split("-"))
