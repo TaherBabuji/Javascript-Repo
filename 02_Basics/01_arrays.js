3f7d3fa3874c9d69b2e9e974c227168f45e41c60
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-11-29 09:03:19 +0000

    arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,36 @@
+// Array
+
+const myArr = [0, 1, 2, 3, 4, 5];
+const myHeroes = ["Iron Man", "Thor", "Captain America"];
+
+const myArr2 = new Array(1, 2, 3, 4, 5);
+// console.log(myArr2[0])
+
+// Array Methods
+
+// myArr.push(6)
+// myArr.push(7)
+// myArr.pop()
+
+// myArr.unshift(9)
+// myArr.shift()
+
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(3));
+
+// const newArr = myArr.join()
+
+// console.log(myArr);
+// console.log(newArr);
+
+// Slice and Splice
+
+console.log("A ", myArr)
+
+console.log("Slice = ",myArr.slice(1, 3))//After using Slice, values inside our aray will not be changed or extracted. It will just copy the elements.
+
+console.log("B ", myArr)
+
+console.log("Splice = ",myArr.splice(1, 3))//After using Splice, values inside our aray will be changed or extracted. It will extract the elements from the array.
+
+console.log("C ", myArr)
