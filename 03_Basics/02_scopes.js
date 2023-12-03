dfbbf54d0593d1e479e70ff8034d9952963ae9a8
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-12-03 06:32:34 +0000

    scopes

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -41,15 +41,15 @@ if (true) {
 
 // console.log(username);
 
-console.log(addOne(5));
+// console.log(addOne(5));
 
 function addOne(num) {
   return num + 1;
 }
 
 
-console.log(addTwo(5));
-
 const addTwo = function (num) {
   return num + 2;
 };
+
+// console.log(addTwo(5));
