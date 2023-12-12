fc6a8eb3c88ed6b2287a6e83e34d65588e60f715
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-12-12 07:58:57 +0000

    iteration

---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+// While loop
+
+let index = 0;
+
+while (index <= 10) {
+  // console.log(`The value of index: ${index} `)
+
+  index += 2;
+}
+
+let myArray = ["Flash", "Batman", "Superman", "Aquaman"];
+
+let i = 0;
+
+while (i < myArray.length) {
+  //   console.log(myArray[i]);
+  i++;
+}
+
+let score = 11;
+
+do {
+  console.log(`The score is ${score}`);
+  score++
+} while (score <= 10);
