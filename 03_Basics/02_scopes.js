601b4eb2f3f8a1683657336c84a3d6d283383307
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-12-18 07:18:31 +0000

    revision

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -1,4 +1,4 @@
-// var c = 300;
+var c = 300;
 let a = 300;
 
 if (true) {
@@ -6,7 +6,7 @@ if (true) {
   const b = 20;
   var c = 30;
 
-  //   console.log("INNER: ", a);
+  // console.log("INNER: ", a);
 }
 
 // console.log(a);
@@ -21,7 +21,7 @@ function one() {
     console.log(username);
   }
 
-  //   console.log(website);
+  // console.log(website);
 
   two();
 }
@@ -36,20 +36,52 @@ if (true) {
     // console.log(username + website);
   }
 
-  //   console.log(website);
+  // console.log(website);
 }
 
 // console.log(username);
 
 // console.log(addOne(5));
 
+// This part of the code is valid. It declares a function named addOne using a function
+// declaration. In JavaScript, function declarations are hoisted, meaning they are moved
+// to the top of the code during the compilation phase. Therefore, you can call addOne
+// before its actual declaration in the code.
+
 function addOne(num) {
   return num + 1;
 }
 
+// console.log(addTwo(5));//This is not going to work because it is declared before initialisation
 
 const addTwo = function (num) {
   return num + 2;
 };
 
+// This part of the code has a problem. It tries to call 'addTwo' before it is declared. The
+// 'addTwo' function is assigned using a function expression, and unlike function declarations,
+// function expressions are not hoisted. Therefore, when the 'console.log(addTwo(5))'; line is
+// encountered, 'addTwo' is not yet defined, resulting in an error.
+
 // console.log(addTwo(5));
+
+// In JavaScript, a function expression is a way to define a function using an expression.
+// Unlike function declarations, function expressions do not get hoisted, which means they
+// are not moved to the top of the code during the compilation phase.
+
+// Function expressions are useful when you want to assign a function to a variable, pass it
+// as an argument to another function, or define functions dynamically. Another common use
+// case for function expressions is creating functions inside objects, especially when
+// defining methods:
+
+const calculator = {
+  add: function (a, b) {
+    return a + b;
+  },
+  sub: function (a, b) {
+    return a - b;
+  },
+};
+
+console.log(calculator.add(4, 5));
+console.log(calculator.sub(5, 3));
--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -5,28 +5,30 @@ const user = {
   price: 999,
 
   welcomeMessage: function () {
-    console.log(`${this.username} , Welcome to website`);
-    console.log(this);
+    console.log(`${this.username}, Welcome to website`);
+    // console.log(this);
   },
 };
 
 // user.welcomeMessage()
-// user.username = "Aliasger"
+user.username = "Aliasger";
 // user.welcomeMessage()
 
-//console.log(this)//This "console.log" will provide an empty object in node but in the browser it will provide "window" object to us.
+// console.log(this)//This "console.log" will provide an empty object in node but in the browser it will provide "window" object to us.
 
-// function chai() {
-//     let username = "Taher"
-//     console.log(this.username)
-// }
+function chai() {
+  let username = "Taher";
+  console.log(this);
+}
 
-// chai()
+// chai();
 
 // const chai =  function() {
 //     let username = "Taher"
 //     console.log(this.username)
 // }
+
+//We can't use "this" keyword inside a function directly
 
 // chai()
 
@@ -39,10 +41,38 @@ const user = {
 
 //IMP. QUE. => Difference between normal function and arrow function.
 
-//const addTwo = (num1, num2) => num1 + num2; //  If we have only 1 line of code inside the function then we can use arrow funtion like this and it is known as implicit return function.
+//1.Arrow function have lexical this scope
+
+// Normal Function:
+// Normal functions have their own "this" binding, which is dynamically scoped and determined
+// by how the function is called. The value of "this" depends on the context in which the
+// function is invoked.
+
+// Arrow Function:
+// Arrow functions do not have their own "this" binding. Instead, they inherit the "this"
+// value from the enclosing scope at the time the function is defined. This behavior makes arrow
+// functions particularly useful in certain situations, such as when defining functions
+// within other functions or callbacks.
+
+//2.We cannot create constructors from arrow functions.
+
+//3.Arrow functions are not hoisted
+
+//4.We cannot access arguements object inside arrow functions. Instead we can use rest
+// operator inside the arrrow function
+
+//5.Way of writing both functions are different
+
+const addTwo = (num1, num2) => {//it is known as explicit return function.
+  return num1 + num2;
+};
+
+// const addTwo = (num1, num2) => num1 + num2; //  If we have only 1 line of code inside the function then we can use arrow funtion like this and it is known as implicit return function.
 
 // const addTwo = (num1, num2) => (num1 + num2);
 
-const addTwo = (num1, num2) => ({ username: "Taher" });
+// const addTwo = (num1, num2) => { username: "Taher" };//We can't return a object without wraping it inside a parenthesis.
+
+// const addTwo = (num1, num2) => ({ username: "Taher" });//When we have to return a object inside a function then we have to use the parenthesis and inside parenthesis we can give object which is going to be returned.
 
 console.log(addTwo(5, 4));
