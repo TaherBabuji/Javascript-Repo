757ae58ce11654492853326296854a7e1b0ce8b4
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-12-19 12:10:28 +0000

    revision

--- 05_iterations/08_reduce_method.js
+++ 05_iterations/08_reduce_method.js
@@ -5,7 +5,7 @@ const myNums = [1, 2, 3];
 //   return acc + currval;
 // }, 0);
 
-const myTotal = myNums.reduce((acc, curr) => acc + curr, 0);
+// const myTotal = myNums.reduce((acc, curr) => acc + curr, 0);
 
 // console.log(myTotal);
 
