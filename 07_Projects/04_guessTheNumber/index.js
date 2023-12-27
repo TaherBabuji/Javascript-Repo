18347c67febf8af27b3f6b2190341cd7fadc1241
TaherBabuji <121304971+TaherBabuji@users.noreply.github.com>
2023-12-27 11:37:55 +0000

    project 4 completed

--- 07_Projects/04_guessTheNumber/index.js
+++ 07_Projects/04_guessTheNumber/index.js
@@ -1,4 +1,4 @@
-const randomNumber = Math.round(Math.random() * 100 + 1);
+let randomNumber = Math.round(Math.random() * 100 + 1);
 console.log(randomNumber);
 
 const userInput = document.querySelector("#guessField");
@@ -6,16 +6,20 @@ const submit = document.querySelector(".guessSubmit");
 const previousGuesses = document.querySelector(".guesses");
 const remainingGuesses = document.querySelector(".lastResult");
 const resultMessage = document.querySelector(".lowOrHi");
+const resultPara = document.querySelector(".resultParas");
 
 let guessedArr = [];
 let numberOfGuesses = 1;
+let playGame = true;
 
-submit.addEventListener("click", (e) => {
-  e.preventDefault();
-  let guess = parseInt(userInput.value);
-  guessValidation(guess);
-  userInput.value = "";
-});
+if (playGame) {
+  submit.addEventListener("click", (e) => {
+    e.preventDefault();
+    let guess = parseInt(userInput.value);
+    guessValidation(guess);
+    userInput.value = "";
+  });
+}
 
 function guessValidation(guess) {
   if (isNaN(guess)) {
@@ -25,34 +29,60 @@ function guessValidation(guess) {
   } else if (guess > 100) {
     alert("Please give number less than 100");
   } else {
-    guessChecking(guess);
-  }
-}
-
-function guessChecking(guess) {
-  if (numberOfGuesses === 10) {
-    gameEnd();
-  } else {
-    if (guess === randomNumber) {
-      resultMessage.innerHTML = `You guessed it right ${guess}`;
-      numberOfGuesses++;
-      remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
-    } else if (guess > randomNumber) {
-      resultMessage.innerHTML = `Number is TOO big ${guess}`;
-      numberOfGuesses++;
-      remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
-      guessedArr.push(guess);
-      previousGuesses.innerHTML = `${guessedArr}`;
-    } else if (guess < randomNumber) {
-      resultMessage.innerHTML = `Number is TOO small ${guess}`;
-      numberOfGuesses++;
-      remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
-      guessedArr.push(guess);
-      previousGuesses.innerHTML = `${guessedArr}`;
+    guessedArr.push(guess);
+    if (numberOfGuesses === 10) {
+      resultMessage.innerHTML = `Your guessing chances are completed <br> The Correct number was: ${randomNumber}`;
+      gameEnd();
+    } else {
+      guessChecking(guess);
     }
   }
 }
 
+function guessChecking(guess) {
+  if (guess === randomNumber) {
+    resultMessage.innerHTML = `You guessed it right: ${guess}`;
+    numberOfGuesses++;
+    remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
+    remainingGuesses.innerHTML = "";
+    gameEnd();
+  } else if (guess > randomNumber) {
+    resultMessage.innerHTML = `Number is TOO big ${guess}`;
+    numberOfGuesses++;
+    remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
+    previousGuesses.innerHTML = `${guessedArr}`;
+  } else if (guess < randomNumber) {
+    resultMessage.innerHTML = `Number is TOO small ${guess}`;
+    numberOfGuesses++;
+    remainingGuesses.innerHTML = `${11 - numberOfGuesses}`;
+    previousGuesses.innerHTML = `${guessedArr}`;
+  }
+}
+
 function gameEnd() {
-  location.reload(true);
+  remainingGuesses.innerHTML = "0";
+  userInput.setAttribute("disabled", "");
+  submit.setAttribute("disabled", "");
+  const endPara = document.createElement("p");
+  endPara.classList.add("button");
+  endPara.innerHTML = `<h2 id="newGame">Start new Game</h2>`;
+  resultPara.append(endPara);
+  playGame = false;
+  gameStart();
+}
+
+function gameStart() {
+  const newGameButton = document.querySelector("#newGame");
+  newGameButton.addEventListener("click", () => {
+    userInput.removeAttribute("disabled");
+    submit.removeAttribute("disabled");
+    resultMessage.innerHTML = "";
+    previousGuesses.innerHTML = "";
+    remainingGuesses.innerHTML = "10";
+    randomNumber = Math.round(Math.random() * 100 + 1);
+    guessedArr = [];
+    numberOfGuesses = 1;
+    playGame = true;
+    newGameButton.innerHTML = "";
+  });
 }
